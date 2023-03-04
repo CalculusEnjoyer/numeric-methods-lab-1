3bc9eea67bfcda77ddbcfdd4ba04ee9a06a7f798
CalculusEnjoyer <kravchuk883@gmail.com>
2023-03-04 13:38:26 +0200

    We don't need no education...

--- src/main/java/calculation/Task3.java
+++ src/main/java/calculation/Task3.java
@@ -1,17 +1,17 @@
 package calculation;
 
-public class Task2 implements EquationsSolver, IterationCalculator {
+public class Task3 implements EquationsSolver, IterationCalculator {
 
   private double accuracy;
   private double startValue;
-  private double left = 0.75;
-  private double right = 1.25;
+  private double left = 1.5;
+  private double right = 2.5;
   private double minOfFirstDerivative = 5.1875;
   private double maxOfSecondDerivative = 13.5;
   private double q;
 
 
-  public Task2(double accuracy, double startValue) {
+  public Task3(double accuracy, double startValue) {
     this.accuracy = accuracy;
     this.startValue = startValue;
     calculateQ();
@@ -20,7 +20,7 @@ public class Task2 implements EquationsSolver, IterationCalculator {
   @Override
   public double findSolution() {
     double result = startValue;
-    for (int i = 0; i < calculateNumberOfIterations(); i++) {
+    for (int i = 0; i < 20; i++) {
       result = recursiveStep(result);
       System.out.println("Value at " + (1 + i) + " step: " + result);
     }
@@ -29,19 +29,12 @@ public class Task2 implements EquationsSolver, IterationCalculator {
 
   @Override
   public int calculateNumberOfIterations() {
-    return 1 + (int) Math.floor(Math.log(Math.log(Math.abs(right - left) / accuracy) / Math.log(1 / q)  + 1) / Math.log(2));
+    return 1 + (int) Math.floor(
+        Math.log(Math.log(Math.abs(right - left) / accuracy) / Math.log(1 / q) + 1) / Math.log(2));
   }
 
   private double recursiveStep(double x) {
-    return x - function(x) / derivativeOfFunction(x);
-  }
-
-  private double function(double x) {
-    return Math.pow(x, 3) + 3 * Math.pow(x, 2) - x - 3;
-  }
-
-  private double derivativeOfFunction(double x) {
-    return 3 * Math.pow(x, 2) + 6 * x - 1;
+    return Math.pow(-Math.pow(x, 2) + 4 * x + 4, 1.0 / 3);
   }
 
   private void calculateQ() {
--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -2,6 +2,7 @@ package org.example;
 
 import calculation.Task1;
 import calculation.Task2;
+import calculation.Task3;
 
 /**
  * Hello world!
@@ -10,11 +11,15 @@ public class App {
 
   public static void main(String[] args) {
     Task1 task1 = new Task1(Math.pow(10, -5), -0.5);
-    System.out.println(task1.calculateNumberOfIterations());
+    System.out.println("Apriori number of iterations: " + task1.calculateNumberOfIterations());
     double result = task1.findSolution();
 
-    Task2 task2 = new Task2(Math.pow(10, -5), 1.2);
-    System.out.println(task2.calculateNumberOfIterations());
+    Task2 task2 = new Task2(Math.pow(10, -8), 0.75);
+    System.out.println("Apriori number of iterations: " + task2.calculateNumberOfIterations());
     result = task2.findSolution();
+
+    Task3 task3 = new Task3(Math.pow(10, -8), 1.5);
+    System.out.println("Apriori number of iterations: " + task3.calculateNumberOfIterations());
+    result = task3.findSolution();
   }
 }
